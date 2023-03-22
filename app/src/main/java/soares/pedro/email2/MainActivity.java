a7541644dea11c3b4e7e2e376a7541b29fa14625
20211tiimi0125 <pedro05042006@gmail.com>
2023-03-22 12:47:53 -0300

    TIREI OS comentarios

--- app/src/main/java/soares/pedro/email2/MainActivity.java
+++ app/src/main/java/soares/pedro/email2/MainActivity.java
@@ -17,25 +17,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //seleciono o botão da interface main
+
         Button btnEnviar =findViewById(R.id.btnEnviar);
-        //crio o evento de clique
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
-            //o que acontece ao clicar no botao
             public void onClick(View v) {
 
-                //seleciono a caixa de texto
+
                 EditText etEmail = findViewById(R.id.etEmail);
                 EditText etAssunto = findViewById(R.id.etAssunto);
                 EditText etTexto = findViewById(R.id.etTexto);
 
-                //armazeno o texto dentro da caixa de texto
                 String email = etEmail.getText().toString();
                 String assunto = etAssunto.getText().toString();
                 String texto = etTexto.getText().toString();
 
-                //passa pra próxima interface
                 Intent i = new Intent(Intent.ACTION_SENDTO);
                 i.setData(Uri.parse("mailto:"));
 
