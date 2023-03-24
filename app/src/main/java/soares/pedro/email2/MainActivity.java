959d72d1c1a249e9308e2bf2d10db8dd34427d57
20211tiimi0125 <pedro05042006@gmail.com>
2023-03-24 09:40:14 -0300

    comentei denovo

--- app/src/main/java/soares/pedro/email2/MainActivity.java
+++ app/src/main/java/soares/pedro/email2/MainActivity.java
@@ -23,23 +23,27 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
+                //seleciono os campos de texto
                 EditText etEmail = findViewById(R.id.etEmail);
                 EditText etAssunto = findViewById(R.id.etAssunto);
                 EditText etTexto = findViewById(R.id.etTexto);
 
+                //pego as informações descritas dentro deles e armazeno em uma variavel
                 String email = etEmail.getText().toString();
                 String assunto = etAssunto.getText().toString();
                 String texto = etTexto.getText().toString();
 
+                //crio uma intenção do tipo mailto
                 Intent i = new Intent(Intent.ACTION_SENDTO);
                 i.setData(Uri.parse("mailto:"));
 
+                //passo as informações armazenadas no intent
                 String[] emails = new String[]{email};
                 i.putExtra(Intent.EXTRA_EMAIL, emails);
                 i.putExtra(Intent.EXTRA_SUBJECT, assunto);
                 i.putExtra(Intent.EXTRA_TEXT, texto);
 
+                //crio um try para caso não haja aplicativo que suporte o mailto
                 try{
                     startActivity(Intent.createChooser(i, "Escolha o APP"));
                 }
